0327c6a45dab35f350e7255674416845d582f7a9
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-09 18:50:18 +0200

    Theme installed and header set

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+
 function App() {
   return (
     <>
-      <h1>Welcome to Mern Shop</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Hello World</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
