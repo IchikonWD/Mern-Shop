45b03de6db4885e16e69ec07129bbc332b5a998c
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-10 20:23:55 +0200

    models created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import { config } from 'dotenv';
+import connectDB from './config/db.js';
 import products from './data/products.js';
+import colors from 'colors';
 
 config();
+
+connectDB();
 
 const app = express();
 
@@ -23,6 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
   );
 });
