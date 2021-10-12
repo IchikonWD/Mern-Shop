42cac311306d1a24d879917e68a8d73894a69c30
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-12 18:30:28 +0200

    Profile controller and routes created

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,6 +3,7 @@ import {
   authUsers,
   getUserProfile,
   registerUser,
+  updateUserProfile,
 } from '../controllers/userController.js';
 import protect from '../middleware/authMiddleware.js';
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post('/', registerUser);
 router.post('/login', authUsers);
 router.get('/profile', protect, getUserProfile);
+router.put('/profile', protect, updateUserProfile);
 
 export default router;
