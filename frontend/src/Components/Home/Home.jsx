63c30c05671370666e19bbe5dd7b2e45621cd8d8
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-14 15:29:21 +0200

    pagination created

--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Row, Col } from 'react-bootstrap';
+import DataTable from 'react-data-table-component';
 import Rating from '../../Components/Rating';
 import Message from '../Message';
 import Loader from '../Loader';
@@ -16,32 +17,64 @@ const Home = () => {
     dispatch(listProducts());
   }, [dispatch]);
 
+  const columns = [
+    {
+      name: 'Image',
+      selector: (row) => row.image,
+      sortable: true,
+    },
+    {
+      name: 'Name',
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: 'Description',
+      selector: (row) => row.description,
+      sortable: true,
+    },
+    {
+      name: 'Rating',
+      selector: (row) => row.rating,
+      sortable: true,
+    },
+    {
+      name: 'Price',
+      selector: (row) => row.price,
+      sortable: true,
+    },
+    {
+      name: 'Stock',
+      selector: (row) => row.stock,
+      sortable: true,
+    },
+  ];
+
+  const tableData = products.map((product) => ({
+    ...product,
+    image: <img src={product.image} alt='product' width='100' height='100' />,
+    name: <Link to={`/product/${product._id}`}>{product.name}</Link>,
+    rating: <Rating value={product.rating} />,
+    price: `${product.price}$`,
+    stock: product.countInStock > 0 ? 'In Stock' : 'Out of Stock',
+  }));
+
   return (
     <>
       <h1>Latest Products</h1>
+
       {loading ? (
         <Loader />
       ) : error ? (
         <Message variant='danger'>{error}</Message>
       ) : (
-        <Row>
-          {products.map((product) => (
-            <Col key={product._id} xs={12} sm={6} md={4} lg={3}>
-              <div className='product-card'>
-                <img src={product.image} alt={product.name} />
-                <Link to={`/product/${product._id}`}>
-                  <h3>{product.name}</h3>
-                </Link>
-                <p>{product.description}</p>
-                <p>{product.price} €</p>
-                <Rating
-                  value={product.rating}
-                  text={`${product.numReviews} reviews`}
-                />
-              </div>
-            </Col>
-          ))}
-        </Row>
+        <DataTable
+          columns={columns}
+          data={tableData}
+          pagination
+          defaultSortFieldId={0}
+          responsive
+        />
       )}
     </>
   );
