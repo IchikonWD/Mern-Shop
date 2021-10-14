133d4d49ff8b9782956a60379254bc7a49a0b237
Ezequiel Migueles <ezequielmigueles@icloud.com>
2021-10-14 23:42:17 +0200

    prepared for production

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,6 @@ connectDB();
 const app = express();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -24,6 +20,18 @@ app.use('/api/orders', orderRoutes);
 app.get('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID);
 });
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
 
 app.use(notFound);
 
